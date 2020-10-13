fb3c1f88f34c0a52d76e80a2198e58fe4a2e7aba
Kota Chang <i@dongju.org>
2020-10-12 19:33:48 -0700

    debuging error : no executables found

---
+++ src/challenge/src/solution.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("solution")
+    {
+      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto vel_msg = geometry_msgs::msg::Twist();
+      vel_msg.linear.x = 1;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", vel_msg.linear.x);
+      publisher_->publish(vel_msg);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  };
+
+  int main(int argc, char * argv[])
+  {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    rclcpp::shutdown();
+    return 0;
+  }
